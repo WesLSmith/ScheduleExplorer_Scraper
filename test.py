97d42fefa63e7114ea096976afd76c46106c2eee
WesLSmith <32778563+WesLSmith@users.noreply.github.com>
2018-01-14 21:57:49 -0500

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import time
 
 ## Testing some sample coordinates
 
-##Input coords can be any sort of Iterable
+##Input coords can be any sort of Iterable, INPUT TIMES IN UTC
 origins = ["42.3563946,-71.0624242", "41.8138752,-71.4245513", "40.6906544,-73.9428026", "40.7216974,-74.0053699", "37.852803,-122.270062"]
 dests = ["42.3422297,-71.0900691", "42.3563946,-71.0624242", "40.6010438,-73.973015", "40.791641,-73.964699", "37.7381014,-122.4689529"]
 u_time = datetime.datetime(2018, 1, 20 , 16, 10,0)
